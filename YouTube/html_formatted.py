8a5432843217425364f0c4b8b34da55e289e4853
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2020-07-30 09:55:56 -0400

    Create html_formatted.py

---
+++ YouTube/html_formatted.py
@@ -0,0 +1,21 @@
+"""
+  COPY AND PASTE CODE FOR YOUTUBE TUTORIAL
+  
+  Video Title: Python Outlook Email - HTML Formatted Email - Learn how to control Microsoft Outlook using Pythonhttps
+  Video URL: //youtu.be/GQkXs-ncS4g
+  Last Modified: 7/30/2020
+"""
+
+html_body = """
+    <div>
+        <h1 style="font-family: 'Lucida Handwriting'; font-size: 56; font-weight: bold; color: #9eac9c;">
+            Happy Birthday!! 
+        </h1>
+        <span style="font-family: 'Lucida Sans'; font-size: 28; color: #8d395c;">
+            Wishing you all the best on your birthday!!
+        </span>
+    </div><br>
+    <div>
+        <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-birthday-instagram-captions-1584723902.jpg" width=50%>
+    </div>
+    """
